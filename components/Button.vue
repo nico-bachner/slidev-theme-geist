f7a685aef369ba0560cef1aa6f98bb64622a5392
Nico Bachner <yo@nicob.dev>
2021-05-13 14:24:16 +0200

    feat(components): added Button component

---
+++ components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+button {
+  color: var(--geist-background);
+  background-color: var(--geist-foreground);
+  border: 1px solid var(--geist-foreground);
+  border-radius: var(--geist-radius);
+  padding: 6px 12px;
+  @apply text-sm;
+}
+button:hover {
+  color: var(--geist-foreground);
+  background-color: var(--geist-background);
+}
+</style>
